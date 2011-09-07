31024e0fe0d512352610d46f0c6c6babc4158c52
Patrick McCormick <pat@ernie.lanl.gov>
2011-09-07 15:20:40 -0600

    Start of a PBO (pixel buffer) aware data transfer to texture OpenGL runtime example in the sandbox.  Fixed a build order issue on systems w/out hwloc pre-installed.

---
+++ include/runtime/opengl/glTextureBuffer.h
@@ -0,0 +1,34 @@
+/*
+ * -----  Scout Programming Language -----
+ *
+ * This file is distributed under an open source license by Los Alamos
+ * National Security, LCC.  See the file License.txt (located in the
+ * top level of the source distribution) for details.
+ * 
+ *-----
+ * 
+ */
+
+#ifndef SCOUT_GL_TEXTURE_BUFFER_H_
+#define SCOUT_GL_TEXTURE_BUFFER_H_
+
+#include "runtime/opengl/glBufferObject.h"
+
+namespace scout
+{
+  // ----- glTextureBuffer
+  //
+  class glTextureBuffer: public glBufferObject {
+    
+   public:
+    glTextureBuffer()
+        : glTextureBuffer(GL_PIXEL_UNPACK_BUFFER)
+    { /* details handled by glBufferObject... */ }
+    
+    ~glTextureBuffer()
+    { /* no-op -- handled by glBufferObject */ }
+
+  };
+}
+
+#endif
