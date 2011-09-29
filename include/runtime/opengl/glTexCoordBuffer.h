d7e219786728951c09915a2310a4bd359ce8caf2
Patrick McCormick <pat@ernie.lanl.gov>
2011-09-29 15:35:17 -0600

    Removed xcode subdir (you can use cmake to build your own xcode project).  Some new OpengL runtime pieces but they need to hammered out more before they're ready to use.  Haven't had a chance to test builds on Linux yet...

---
+++ include/runtime/opengl/glTexCoordBuffer.h
@@ -0,0 +1,41 @@
+/*
+ *           -----  The Scout Programming Language -----
+ *
+ * This file is distributed under an open source license by Los Alamos
+ * National Security, LCC.  See the file License.txt (located in the
+ * top level of the source distribution) for details.
+ * 
+ *-----
+ *
+ * $Revision$
+ * $Date$
+ * $Author$
+ *
+ *-----
+ * 
+ */
+
+#ifndef SCOUT_OPENGL_TEXCOORD_BUFFER_H_
+#define SCOUT_OPENGL_TEXCOORD_BUFFER_H_
+
+#include "runtime/opengl/glBufferObject.h"
+
+namespace scout
+{
+  
+  // ---- glTexCoordBuffer
+  //
+  class glTexCoordBuffer: public glBufferObject {
+    
+   public:
+    glTexCoordBuffer()
+        : glBufferObject(GL_ARRAY_BUFFER)
+    { /* everything else is handled by glBufferObject... */ }
+    
+    ~glTexCoordBuffer()
+    { /* no-op */ }
+
+  };
+}
+
+#endif
