581d28a2c3f8700569d1733f0fa6f99d1e43ff31
Jamie Jablin <jjablin@grover.lanl.gov>
2011-11-08 11:44:26 -0700

    Update cshift to support up to 3D meshes.

--- llvm/tools/clang/lib/CodeGen/CGExpr.cpp
+++ llvm/tools/clang/lib/CodeGen/CGExpr.cpp
@@ -2619,28 +2619,31 @@ LValue CodeGenFunction::EmitMeshMemberExpr(const VarDecl *VD, llvm::StringRef me
       dims.push_back(llvm::ConstantInt::get(i32Ty, dim));
     }
 
-    for(unsigned i = dims.size(); i < 2; ++i) {
+    for(unsigned i = dims.size(); i < 3; ++i) {
       dims.push_back(llvm::ConstantInt::get(i32Ty, 1));
     }
 
-    for(unsigned i = vals.size(); i < 2; ++i) {
+    for(unsigned i = vals.size(); i < 3; ++i) {
       vals.push_back(llvm::ConstantInt::get(i32Ty, 0));
     }
 
-    llvm::Value *pred = Builder.CreateICmpSLT(vals[0], zero);
-    vals[0] = Builder.CreateSelect(pred, Builder.CreateAdd(vals[0], dims[0]), vals[0]);
-
-    pred = Builder.CreateICmpSLT(vals[1], zero);
-    vals[1] = Builder.CreateSelect(pred, Builder.CreateAdd(vals[1], dims[1]), vals[1]);
-
-    llvm::Value *indvar = getGlobalIdx();
-    llvm::Value *sum1 = Builder.CreateURem(Builder.CreateAdd(indvar, vals[0]), dims[0]);
-    llvm::Value *sum2 = Builder.CreateMul(Builder.CreateSDiv(indvar, dims[0]), dims[0]);
-    llvm::Value *sum3 = Builder.CreateMul(vals[1], dims[0]);
-    llvm::Value *sum = Builder.CreateAdd(sum1, sum2);
-    sum = Builder.CreateAdd(sum, sum3);
-
-    arg = Builder.CreateURem(sum, Builder.CreateMul(dims[0], dims[1]));
+    llvm::Value *idx   = getGlobalIdx();
+    llvm::Value *add   = Builder.CreateAdd(idx, vals[0]);
+    llvm::Value *rem   = Builder.CreateURem(add, dims[0]);
+    llvm::Value *div   = Builder.CreateUDiv(idx, dims[0]);
+    llvm::Value *rem1  = Builder.CreateURem(div, dims[1]);
+    llvm::Value *add2  = Builder.CreateAdd(rem1, vals[1]);
+    llvm::Value *rem3  = Builder.CreateURem(add2, dims[1]);
+    llvm::Value *mul   = Builder.CreateMul(dims[0], dims[1]);
+    llvm::Value *div4  = Builder.CreateUDiv(idx, mul);
+    llvm::Value *rem5  = Builder.CreateURem(div4, dims[2]);
+    llvm::Value *fac   = Builder.CreateShl(vals[2], llvm::ConstantInt::get(i32Ty, 1));
+    llvm::Value *add7  = Builder.CreateAdd(fac, rem5);
+    llvm::Value *rem8  = Builder.CreateURem(add7, dims[2]);
+    llvm::Value *mul12 = Builder.CreateMul(rem8, dims[1]);
+    llvm::Value *tmp   = Builder.CreateAdd(mul12, rem3);
+    llvm::Value *tmp1  = Builder.CreateMul(tmp, dims[0]);
+    arg = Builder.CreateAdd(tmp1, rem);
   }
   llvm::Value *var = MeshMembers[memberName].first;
   QualType Ty = MeshMembers[memberName].second;
