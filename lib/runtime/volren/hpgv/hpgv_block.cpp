84f7f395b077cf785df6e0e45b57596eeff3a806
Christine Ahrens <cahrens@lanl.gov>
2012-03-06 16:42:40 -0700

    Fix arg to MPI_Get_count.

--- lib/runtime/volren/hpgv/hpgv_block.cpp
+++ lib/runtime/volren/hpgv/hpgv_block.cpp
@@ -1594,7 +1594,7 @@ void block_read_data(block_t *block, int vol, char *filename)
     int count; 
     MPI_Get_count(&status, datatype, &count);
     HPGV_ASSERT_P(block->mpiid,
-                  count == hpgv_typesize(volume->data_type) * totalsize,
+                  count == totalsize,
                   "Inconsistent read",
                   HPGV_ERR_IO);
     
@@ -1711,7 +1711,7 @@ void block_write_data(block_t *block, int vol, char *filename)
     MPI_Get_count(&status, datatype, &count);
 
     HPGV_ASSERT_P(block->mpiid,
-                  count == hpgv_typesize(HPGV_FLOAT) * totalsize,
+                  count == totalsize,
                   "Inconsistent read",
                   HPGV_ERR_IO);
     
