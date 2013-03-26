b7f895d0087833aded1c781fd1adeb282d76e9a6
dean prichard <dpx@lanl.gov>
2013-03-26 13:19:41 -0600

    working on renderall_uniform

--- lib/Runtime/renderall/opencl/opencl_renderall_uniform.cpp
+++ lib/Runtime/renderall/opencl/opencl_renderall_uniform.cpp
@@ -1,3 +1,57 @@
+/*
+ * ###########################################################################
+ * Copyright (c) 2010, Los Alamos National Security, LLC.
+ * All rights reserved.
+ * 
+ *  Copyright 2010. Los Alamos National Security, LLC. This software was
+ *  produced under U.S. Government contract DE-AC52-06NA25396 for Los
+ *  Alamos National Laboratory (LANL), which is operated by Los Alamos
+ *  National Security, LLC for the U.S. Department of Energy. The
+ *  U.S. Government has rights to use, reproduce, and distribute this
+ *  software.  NEITHER THE GOVERNMENT NOR LOS ALAMOS NATIONAL SECURITY,
+ *  LLC MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR ASSUMES ANY LIABILITY
+ *  FOR THE USE OF THIS SOFTWARE.  If software is modified to produce
+ *  derivative works, such modified software should be clearly marked,
+ *  so as not to confuse it with the version available from LANL.
+ *
+ *  Additionally, redistribution and use in source and binary forms,
+ *  with or without modification, are permitted provided that the
+ *  following conditions are met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ * 
+ *    * Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided 
+ *      with the distribution.
+ *
+ *    * Neither the name of Los Alamos National Security, LLC, Los
+ *      Alamos National Laboratory, LANL, the U.S. Government, nor the
+ *      names of its contributors may be used to endorse or promote
+ *      products derived from this software without specific prior
+ *      written permission.
+ * 
+ *  THIS SOFTWARE IS PROVIDED BY LOS ALAMOS NATIONAL SECURITY, LLC AND
+ *  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL LOS ALAMOS NATIONAL SECURITY, LLC OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ *  USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ *  OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ *  SUCH DAMAGE.
+ * ########################################################################### 
+ * 
+ * Notes
+ *
+ * ##### 
+ */ 
+
 #include <cassert>
 #include "scout/Runtime/base_types.h"
 #include "scout/Runtime/opengl/glSDL.h"
@@ -6,16 +60,49 @@
 #include "scout/Runtime/opencl/scout_opencl.h"
 #include "scout/Runtime/renderall/renderall_uniform_.h"
 
+using namespace scout;
+cl_mem __sc_opencl_device_renderall_uniform_colors;
+extern float4* __sc_renderall_uniform_colors;
+
 namespace scout{
+
   void renderall_uniform_rt_::register_pbo(GLuint pbo){
     cl_int ret;
     if(__sc_opencl) {
       __sc_opencl_device_renderall_uniform_colors =
           clCreateFromGLBuffer(__sc_opencl_context,
-                           CL_MEM_WRITE_ONLY,
-                           pbo,
-                           &ret);
+              CL_MEM_WRITE_ONLY,
+              pbo,
+              &ret);
       assert(ret == CL_SUCCESS);
+    }
+  }
+
+  void renderall_uniform_rt_::map_gpu_resources(void) {
+    if(__sc_opencl) {
+      glFinish();
+      clEnqueueAcquireGLObjects(__sc_opencl_command_queue,
+          1,
+          &__sc_opencl_device_renderall_uniform_colors,
+          0,
+          NULL,
+          NULL);
+    } else {
+      __sc_renderall_uniform_colors =_renderable->map_colors();
+    }
+  }
+
+  void renderall_uniform_rt_::unmap_gpu_resources(void) {
+    if(__sc_opencl) {
+      clEnqueueReleaseGLObjects(__sc_opencl_command_queue,
+          1,
+          &__sc_opencl_device_renderall_uniform_colors,
+          0,
+          NULL,
+          NULL);
+      clFinish(__sc_opencl_command_queue);
+    } else {
+      _renderable->unmap_colors();
     }
   }
 }
--- lib/Runtime/renderall/renderall_uniform.cpp
+++ lib/Runtime/renderall/renderall_uniform.cpp
@@ -1,14 +1,56 @@
 /*
- * -----  Scout Programming Language -----
+ * ###########################################################################
+ * Copyright (c) 2010, Los Alamos National Security, LLC.
+ * All rights reserved.
+ * 
+ *  Copyright 2010. Los Alamos National Security, LLC. This software was
+ *  produced under U.S. Government contract DE-AC52-06NA25396 for Los
+ *  Alamos National Laboratory (LANL), which is operated by Los Alamos
+ *  National Security, LLC for the U.S. Department of Energy. The
+ *  U.S. Government has rights to use, reproduce, and distribute this
+ *  software.  NEITHER THE GOVERNMENT NOR LOS ALAMOS NATIONAL SECURITY,
+ *  LLC MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR ASSUMES ANY LIABILITY
+ *  FOR THE USE OF THIS SOFTWARE.  If software is modified to produce
+ *  derivative works, such modified software should be clearly marked,
+ *  so as not to confuse it with the version available from LANL.
  *
- * This file is distributed under an open source license by Los Alamos
- * National Security, LCC.  See the file License.txt (located in the
- * top level of the source distribution) for details.
+ *  Additionally, redistribution and use in source and binary forms,
+ *  with or without modification, are permitted provided that the
+ *  following conditions are met:
  *
- * -----
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ * 
+ *    * Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided 
+ *      with the distribution.
  *
- */
-
+ *    * Neither the name of Los Alamos National Security, LLC, Los
+ *      Alamos National Laboratory, LANL, the U.S. Government, nor the
+ *      names of its contributors may be used to endorse or promote
+ *      products derived from this software without specific prior
+ *      written permission.
+ * 
+ *  THIS SOFTWARE IS PROVIDED BY LOS ALAMOS NATIONAL SECURITY, LLC AND
+ *  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL LOS ALAMOS NATIONAL SECURITY, LLC OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ *  USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ *  OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ *  SUCH DAMAGE.
+ * ########################################################################### 
+ * 
+ * Notes
+ *
+ * ##### 
+ */ 
 
 #include <iostream>
 #include "scout/Runtime/renderall/renderall_uniform.h"
@@ -20,34 +62,12 @@
 // scout includes
 #include "scout/Config/defs.h"
 
-#ifdef SC_ENABLE_CUDA
-#include <cuda.h>
-#include <cudaGL.h>
-#include "scout/Runtime/cuda/CudaDevice.h"
-#endif
-
-#ifdef SC_ENABLE_OPENCL
-#include "scout/Runtime/opencl/scout_opencl.h"
-#endif
-
 using namespace std;
 using namespace scout;
 
 // ------  LLVM - globals accessed by LLVM / CUDA driver
 
 float4* __sc_renderall_uniform_colors;
-
-#ifdef SC_ENABLE_CUDA
-CUdeviceptr __sc_cuda_device_renderall_uniform_colors;
-#else
-static bool __sc_cuda = false;
-#endif
-
-#ifdef SC_ENABLE_OPENCL
-cl_mem __sc_opencl_device_renderall_uniform_colors;
-#else
-static bool __sc_opencl = false;
-#endif
 
 // -------------
 
@@ -59,144 +79,54 @@ void __sc_init_sdl(size_t width, size_t height, glCamera* camera = NULL);
 
 namespace scout{
 
-    renderall_uniform_rt_::renderall_uniform_rt_(renderall_uniform_rt* o)
-        : o_(o){
+  renderall_uniform_rt_::renderall_uniform_rt_(renderall_uniform_rt* o)
+  : o_(o){
 
 
-          if(!__sc_glsdl){
-            __sc_init_sdl(__sc_initial_width, __sc_initial_height);
-          }
+    if(!__sc_glsdl){
+      __sc_init_sdl(__sc_initial_width, __sc_initial_height);
+    }
 
-          init();
-        }
+    init();
+  }
 
-    renderall_uniform_rt_::~renderall_uniform_rt_(){
-        if (_renderable != NULL) delete _renderable;
-      }
+  renderall_uniform_rt_::~renderall_uniform_rt_(){
+    if (_renderable != NULL) delete _renderable;
+  }
 
-      void renderall_uniform_rt_::init(){
-        _renderable = new glQuadRenderableVA( glfloat3(0.0, 0.0, 0.0),
-          glfloat3(o_->width(), o_->height(), 0.0));
+  void renderall_uniform_rt_::init(){
+    _renderable = new glQuadRenderableVA( glfloat3(0.0, 0.0, 0.0),
+        glfloat3(o_->width(), o_->height(), 0.0));
 
-        register_pbo(_renderable->get_buffer_object_id());
+    register_pbo(_renderable->get_buffer_object_id());
 
-        _renderable->initialize(NULL);
+    _renderable->initialize(NULL);
 
-        // show empty buffer
-        __sc_glsdl->swapBuffers();
-      }
+    // show empty buffer
+    __sc_glsdl->swapBuffers();
+  }
 
-      void renderall_uniform_rt_::begin(){
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-#if defined(SC_ENABLE_CUDA) || defined(SC_ENABLE_OPENCL)
-        if(__sc_cuda){
-          map_cuda_resources();
-        }
-        else if(__sc_opencl){
-          map_opencl_resources();
-        }
-        else{
-         __sc_renderall_uniform_colors =_renderable->map_colors();
-        }
-#else
-       __sc_renderall_uniform_colors =_renderable->map_colors(); 
-#endif
-      }
+  void renderall_uniform_rt_::begin(){
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    map_gpu_resources();
+  }
 
-      void renderall_uniform_rt_::end(){
-#if defined(SC_ENABLE_CUDA) || defined(SC_ENABLE_OPENCL)
-        if(__sc_cuda){
-          unmap_cuda_resources();
-        }
-        else if(__sc_opencl){
-          unmap_opencl_resources();
-        }
-        else{
-          _renderable->unmap_colors();
-        }
-#else
-        _renderable->unmap_colors(); 
-#endif
+  void renderall_uniform_rt_::end(){
+    unmap_gpu_resources();
 
-        exec();
+    exec();
 
-        // show what we just drew
-        __sc_glsdl->swapBuffers();
+    // show what we just drew
+    __sc_glsdl->swapBuffers();
 
-        bool done = __sc_glsdl->processEvent();
+    bool done = __sc_glsdl->processEvent();
 
-        if (done) exit(0);
+    if (done) exit(0);
 
-      }
-
-      void renderall_uniform_rt_::map_cuda_resources(){
-#ifdef SC_ENABLE_CUDA
-        // map one graphics resource for access by CUDA
-        assert(cuGraphicsMapResources(1, &__sc_cuda_device_resource, 0) == CUDA_SUCCESS);
-
-        size_t bytes;
-        // return a pointer by which the mapped graphics resource may be accessed.
-        assert(cuGraphicsResourceGetMappedPointer(&__sc_cuda_device_renderall_uniform_colors, &bytes, __sc_cuda_device_resource) == CUDA_SUCCESS);
-#endif
-      }
-
-      void renderall_uniform_rt_::unmap_cuda_resources(){
-#ifdef SC_ENABLE_CUDA
-        assert(cuGraphicsUnmapResources(1, &__sc_cuda_device_resource, 0) == CUDA_SUCCESS);
-
-        _renderable->alloc_texture();
-#endif
-      }
-
-      void renderall_uniform_rt_::map_opencl_resources(){
-#ifdef SC_ENABLE_OPENCL
-        glFinish();
-        clEnqueueAcquireGLObjects(__sc_opencl_command_queue, 
-                                  1, 
-                                  &__sc_opencl_device_renderall_uniform_colors,
-                                  0,
-                                  NULL,
-                                  NULL);
-#endif
-      }
-
-      void renderall_uniform_rt_::unmap_opencl_resources(){
-#ifdef SC_ENABLE_OPENCL
-        clEnqueueReleaseGLObjects(__sc_opencl_command_queue,
-                                  1,
-                                  &__sc_opencl_device_renderall_uniform_colors,
-                                  0,
-                                  NULL,
-                                  NULL);
-        clFinish(__sc_opencl_command_queue);
-#endif
-      }
-
-      // register pbo for access by CUDA, return handle 
-      void renderall_uniform_rt_::register_cuda_pbo(GLuint pbo){
-#ifdef SC_ENABLE_CUDA
-        assert(cuGraphicsGLRegisterBuffer(&__sc_cuda_device_resource, 
-                                          pbo, 
-                                          CU_GRAPHICS_REGISTER_FLAGS_WRITE_DISCARD) == CUDA_SUCCESS);
-#endif
-      }
-
-      // register pbo for access by CUDA, return handle 
-      void renderall_uniform_rt_::register_opencl_pbo(GLuint pbo){
-#ifdef SC_ENABLE_OPENCL
-        cl_int ret;
-        __sc_opencl_device_renderall_uniform_colors = 
-          clCreateFromGLBuffer(__sc_opencl_context,
-                               CL_MEM_WRITE_ONLY,
-                               pbo,
-                               &ret);
-        assert(ret == CL_SUCCESS);
-#endif
-      }
-
-      void renderall_uniform_rt_::exec(){
-        _renderable->draw(NULL);
-      }
+  }
+  void renderall_uniform_rt_::exec(){
+    _renderable->draw(NULL);
+  }
 
 } // end namespace scout
 
