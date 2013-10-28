3ea7c78fb9db77868c2d9b6f90d3e96b461aae6c
Patrick McCormick <mccormick.ps@gmail.com>
2013-10-27 20:00:00 -0600

    Fixed syntax error...

--- git-tools/update-clang-subtree.sh
+++ git-tools/update-clang-subtree.sh
@@ -14,7 +14,7 @@
 # you pass compile and regression checks).  However, we'll leave this
 # as a sanity check...
 branch=$(active_git_branch)
-if [ $branch != 'devel' ] && [ $branch != 'overhaul']; then 
+if [ $branch != 'devel' ] && [ $branch != 'overhaul' ]; then 
     echo "This script must be run on the 'devel' or 'overhaul' branch."
     exit 1;
 fi
