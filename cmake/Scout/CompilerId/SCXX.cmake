b7baad7f18b730c9eaa9bba59bba3b61327aaf39
dean prichard <dpx@lanl.gov>
2013-12-09 09:32:50 -0700

    remove -disable-sc-stdlib flag

--- cmake/Scout/CompilerId/SCXX.cmake
+++ cmake/Scout/CompilerId/SCXX.cmake
@@ -2,12 +2,6 @@ include(Compiler/GNU)
 __compiler_gnu(CXX)
 
 if(CMAKE_SCXX_BOOTSTRAP)
-
-  set(CMAKE_SCXX_FLAGS ${CMAKE_SCXX_FLAGS} 
-    ${CMAKE_CXX_FLAGS} 
-    -disable-sc-stdlib)
-
   set(CMAKE_SCXX_LINK_FLAGS ${CMAKE_SCXX_LINK_FLAGS} 
     ${CMAKE_CXX_LINK_FLAGS} -noscstdlib)
-
 endif()
