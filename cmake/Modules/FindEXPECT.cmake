5323d5030e96ae3910a829f04938bab8a03d1450
dpx <dpx@lanl.gov>
2014-04-23 16:39:18 -0600

    Add tests for debug scripts, make sure we can set a breakpoint in a scout directory. Need to export DEBUG_TESTS=1 to run

---
+++ cmake/Modules/FindEXPECT.cmake
@@ -0,0 +1,27 @@
+set(EXPECT_ROOT_DIR
+	"${EXPECT_ROOT_DIR}"
+	CACHE
+	PATH
+	"Directory to start our search in")
+
+find_program(EXPECT_COMMAND
+	NAMES
+	lldb
+	HINTS
+	"${EXPECT_ROOT_DIR}"
+	PATH_SUFFIXES
+	bin
+	libexec)
+
+if(EXPECT_COMMAND)
+	execute_process(COMMAND EXPECT -version
+		COMMAND head -n 1
+		OUTPUT_VARIABLE EXPECT_VERSION
+		OUTPUT_STRIP_TRAILING_WHITESPACE)
+	string(REGEX REPLACE "[^0-9]*([0-9]+[0-9.]*).*" "\\1" EXPECT_VERSION "${EXPECT_VERSION}")
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(EXPECT DEFAULT_MSG EXPECT_COMMAND EXPECT_VERSION)
+
+mark_as_advanced(EXPECT_COMMAND)
---
+++ test/lldb/breakpoint.sc
@@ -0,0 +1,1 @@
+../scc/cells.sc
