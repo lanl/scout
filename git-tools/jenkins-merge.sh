1a7031fcbe8839e164031e9d315d784a10ff6753
Dean Prichard <dpx@lanl.gov>
2014-05-20 16:05:19 -0600

    add lldb to jenkins merge job, also try and do clang even if llvm fails so we can see conflicts

--- git-tools/jenkins-merge.sh
+++ git-tools/jenkins-merge.sh
@@ -1,8 +1,11 @@
 #!/bin/sh 
 #
-# script for jenkins to try and merge w/ llvm/clang
+# script for jenkins to try and merge w/ llvm/clang/lldb
 # and generate a list of conflicts
 
+ret=0
+
+# try llvm merge
 mkdir -p llvmlog
 echo "<pre>" > llvmlog/index.html
 ./git-tools/update-llvm-subtree.sh >> llvmlog/index.html 2>&1
@@ -14,24 +17,38 @@ then
   while read f; do
     git diff HEAD HEAD~1 $f
   done < /tmp/conflicts
-  exit 1
-else
-  #try and merge w/ clang
-  mkdir -p clanglog
-  echo "<pre>" > clanglog/index.html
-  ./git-tools/update-clang-subtree.sh >> clanglog/index.html 2>&1
-  if [ $? -ne 0 ]
-  then
-    # get list of conflicts
-    git diff --name-only --diff-filter=U > /tmp/conflicts
-    git commit -am "commit w/ conflicts"
-    while read f; do
-      git diff HEAD HEAD~1 $f
-    done < /tmp/conflicts
-    exit 1
-  else
-    exit 0
-  fi
+  ret=$((ret+1))
 fi
-#
 
+#try and merge w/ clang
+mkdir -p clanglog
+echo "<pre>" > clanglog/index.html
+./git-tools/update-clang-subtree.sh >> clanglog/index.html 2>&1
+if [ $? -ne 0 ]
+then
+  # get list of conflicts
+  git diff --name-only --diff-filter=U > /tmp/conflicts
+  git commit -am "commit w/ conflicts"
+  while read f; do
+    git diff HEAD HEAD~1 $f
+  done < /tmp/conflicts
+  ret=$((ret+1))
+fi
+
+#try and merge w/ lldb
+mkdir -p lldblog
+echo "<pre>" > lldblog/index.html
+./git-tools/update-lldb-subtree.sh >> lldblog/index.html 2>&1
+if [ $? -ne 0 ]
+then
+  # get list of conflicts
+  git diff --name-only --diff-filter=U > /tmp/conflicts
+  git commit -am "commit w/ conflicts"
+  while read f; do
+    git diff HEAD HEAD~1 $f
+  done < /tmp/conflicts
+  ret=$((ret+1))
+fi
+
+return $ret
+#
