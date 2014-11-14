a76359bbefcbc12d3bd01e763d7b560b73804bbe
Pat McCormick <pat@lanl.gov>
2014-11-13 22:28:59 -0700

    Working on lldb bugs with prefix header (hsv, hsva call issues).

--- tools/lldb/lldb-prefix.h
+++ tools/lldb/lldb-prefix.h
@@ -1,6 +1,7 @@
 typedef float float4 __attribute__((ext_vector_type(4)));
+float4 hsva(float, float, float, float);
+float4 hsv(float, float, float);
 
 extern "C" {
-  float4 hsva(float, float, float, float);
-  float4 hsv(float, float, float);
+  void printf(const char *, ...);
 }
