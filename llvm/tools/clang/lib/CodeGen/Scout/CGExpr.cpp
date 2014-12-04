0e9df32153f2818ae6b8d870f32f8756c2037373
Dean Prichard <dpx@lanl.gov>
2014-12-04 16:21:49 -0700

    more work on head/tail for edges

--- llvm/tools/clang/lib/CodeGen/Scout/CGExpr.cpp
+++ llvm/tools/clang/lib/CodeGen/Scout/CGExpr.cpp
@@ -594,92 +594,210 @@ RValue CodeGenFunction::EmitMeshParameterExpr(const Expr *E, MeshParameterOffset
 
 RValue CodeGenFunction::EmitTailExpr(void) {
 
+  assert(EdgeIndex && "no edges in call to tail");
+
   llvm::Value* Zero = llvm::ConstantInt::get(Int32Ty, 0);
   llvm::Value* One = llvm::ConstantInt::get(Int32Ty, 1);
   llvm::Value* Two = llvm::ConstantInt::get(Int32Ty, 2);
   llvm::Value* Three = llvm::ConstantInt::get(Int32Ty, 3);
 
-  if(EdgeIndex) {
+  llvm::Value *Rank = Builder.CreateLoad(MeshRank);
+  llvm::Value *Index = Builder.CreateLoad(EdgeIndex, "forall.edgeIndex");
+  llvm::Value *Dx = Builder.CreateLoad(MeshDims[0]);
+  llvm::Value *Dx1 = Builder.CreateAdd(Dx,One);
+  llvm::Value *Dy = Builder.CreateLoad(MeshDims[1]);
+  llvm::Value *Dy1 = Builder.CreateAdd(Dy,One);
 
-    llvm::Value *Rank = Builder.CreateLoad(MeshRank);
+  llvm::BasicBlock *Then3 = createBasicBlock("rank3.then");
+  llvm::BasicBlock *Else3 = createBasicBlock("rank3.else");
+  llvm::BasicBlock *Then2 = createBasicBlock("rank2.then");
+  llvm::BasicBlock *Else2 = createBasicBlock("rank2.else");
+  llvm::BasicBlock *Merge = createBasicBlock("rank.merge");
 
-    llvm::BasicBlock *Then3 = createBasicBlock("rank3.then");
-    llvm::BasicBlock *Else3 = createBasicBlock("rank3.else");
-    llvm::BasicBlock *Then2 = createBasicBlock("rank2.then");
-    llvm::BasicBlock *Else2 = createBasicBlock("rank2.else");
-    llvm::BasicBlock *Merge = createBasicBlock("rank.merge");
+  llvm::Value *Check3 = Builder.CreateICmpEQ(Rank, Three);
+  Builder.CreateCondBr(Check3, Then3, Else3);
 
-    llvm::Value *Check3 = Builder.CreateICmpEQ(Rank, Three);
-    Builder.CreateCondBr(Check3, Then3, Else3);
+  // rank = 3
+  EmitBlock(Then3);
+  llvm::Value *Result3 =
+      llvm::UndefValue::get(llvm::VectorType::get(Int32Ty, 4));
+  //SC_TOD0: add correct x,y,z
+  Result3 = Builder.CreateInsertElement(Result3, Zero,
+      Builder.getInt32(0), "tail.x");
+  Result3 = Builder.CreateInsertElement(Result3, Zero,
+      Builder.getInt32(1), "tail.y");
+  Result3 = Builder.CreateInsertElement(Result3, Zero,
+      Builder.getInt32(2), "tail.z");
+  Result3 = Builder.CreateInsertElement(Result3, Index,
+      Builder.getInt32(3), "tail.idx");
 
-    // rank = 3
-    EmitBlock(Then3);
-    llvm::Value *Result3 =
-                  llvm::UndefValue::get(llvm::VectorType::get(Int32Ty, 4));
-    //SC_TOD0: add correct x,y,z
-    Result3 = Builder.CreateInsertElement(Result3, Zero,
-        Builder.getInt32(0), "tail.x");
-    Result3 = Builder.CreateInsertElement(Result3, Zero,
-        Builder.getInt32(1), "tail.y");
-    Result3 = Builder.CreateInsertElement(Result3, Zero,
-        Builder.getInt32(2), "tail.z");
-    Result3 = Builder.CreateInsertElement(Result3, Builder.CreateLoad(EdgeIndex, "forall.edgeIndex"),
-        Builder.getInt32(3),  "tail.idx");
+  Builder.CreateBr(Merge);
 
-    //SC_TODO
-    Builder.CreateBr(Merge);
+  // rank != 3
+  EmitBlock(Else3);
 
-    // rank != 3
-    EmitBlock(Else3);
+  // rank = 2
+  llvm::Value *Check2 = Builder.CreateICmpEQ(Rank, Two);
+  Builder.CreateCondBr(Check2, Then2, Else2);
+  EmitBlock(Then2);
+  llvm::Value *Result2 =
+      llvm::UndefValue::get(llvm::VectorType::get(Int32Ty, 4));
 
-    // rank = 2
-    llvm::Value *Check2 = Builder.CreateICmpEQ(Rank, Two);
-    Builder.CreateCondBr(Check2, Then2, Else2);
-    EmitBlock(Then2);
-    llvm::Value *Result2 =
-                  llvm::UndefValue::get(llvm::VectorType::get(Int32Ty, 4));
+  // number of edges in x direction
+  llvm::Value *nx = Builder.CreateMul(Dx1, Dy);
+  // what is direction for this Index
+  llvm::Value *dir = Builder.CreateICmpSLT(Index, nx);
+  llvm::Value *Index2 = Builder.CreateSub(Index, nx);
 
-    //SC_TOD0: add correct x,y
-    Result2 = Builder.CreateInsertElement(Result2, Zero,
-        Builder.getInt32(0), "tail.x");
-    Result2 = Builder.CreateInsertElement(Result2, Zero,
-        Builder.getInt32(1), "tail.y");
-    Result2 = Builder.CreateInsertElement(Result2, Zero,
-        Builder.getInt32(2), "tail.z");
-    Result2 = Builder.CreateInsertElement(Result2, Builder.CreateLoad(EdgeIndex, "forall.edgeIndex"),
-        Builder.getInt32(3),  "tail.idx");
+  // for edges in x direction
+  llvm::Value *x1 = Builder.CreateSRem(Index, Dx1);
+  llvm::Value *y1 = Builder.CreateSRem(Builder.CreateSDiv(Index, Dx1), Dy);
+  // for edges in y direction
+  llvm::Value *x2 = Builder.CreateSRem(Index2, Dx);
+  llvm::Value *y2 = Builder.CreateSRem(Builder.CreateSDiv(Index2, Dx), Dy1);
 
-    Builder.CreateBr(Merge);
+  llvm::Value *x = Builder.CreateSelect(dir, x1, x2);
+  llvm::Value *y = Builder.CreateSelect(dir, y1, y2);
 
-    // rank !=2 (rank = 1)
-    EmitBlock(Else2);
-    llvm::Value *Result1 =
-              llvm::UndefValue::get(llvm::VectorType::get(Int32Ty, 4));
-    llvm::Value *x = Builder.CreateLoad(EdgeIndex, "forall.edgeIndex");
-        Result1 = Builder.CreateInsertElement(Result1, x,
-            Builder.getInt32(0), "tail.x");
-        Result1 = Builder.CreateInsertElement(Result1, Zero,
-            Builder.getInt32(1), "tail.y");
-        Result1 = Builder.CreateInsertElement(Result1, Zero,
-            Builder.getInt32(2), "tail.z");
-        Result1 = Builder.CreateInsertElement(Result1, x,
-            Builder.getInt32(3),  "tail.idx");
+  Result2 = Builder.CreateInsertElement(Result2, x,
+      Builder.getInt32(0), "tail.x");
+  Result2 = Builder.CreateInsertElement(Result2, y,
+      Builder.getInt32(1), "tail.y");
+  Result2 = Builder.CreateInsertElement(Result2, Zero,
+      Builder.getInt32(2), "tail.z");
+  Result2 = Builder.CreateInsertElement(Result2, Index,
+      Builder.getInt32(3), "tail.idx");
 
-    Builder.CreateBr(Merge);
+  Builder.CreateBr(Merge);
 
-    // Merge Block
-    EmitBlock(Merge);
-    llvm::PHINode *PN = Builder.CreatePHI(llvm::VectorType::get(Int32Ty, 4), 3, "tail.phi");
-    PN->addIncoming(Result3, Then3);
-    PN->addIncoming(Result2, Then2);
-    PN->addIncoming(Result1, Else2);
+  // rank !=2 (rank = 1)
+  EmitBlock(Else2);
+  llvm::Value *Result1 =
+      llvm::UndefValue::get(llvm::VectorType::get(Int32Ty, 4));
+  Result1 = Builder.CreateInsertElement(Result1, Index,
+      Builder.getInt32(0), "tail.x");
+  Result1 = Builder.CreateInsertElement(Result1, Zero,
+      Builder.getInt32(1), "tail.y");
+  Result1 = Builder.CreateInsertElement(Result1, Zero,
+      Builder.getInt32(2), "tail.z");
+  Result1 = Builder.CreateInsertElement(Result1, Index,
+      Builder.getInt32(3), "tail.idx");
 
-    return RValue::get(PN);
-  }
+  Builder.CreateBr(Merge);
 
+  // Merge Block
+  EmitBlock(Merge);
+  llvm::PHINode *PN = Builder.CreatePHI(llvm::VectorType::get(Int32Ty, 4), 3, "tail.phi");
+  PN->addIncoming(Result3, Then3);
+  PN->addIncoming(Result2, Then2);
+  PN->addIncoming(Result1, Else2);
+
+  return RValue::get(PN);
 }
 
 RValue CodeGenFunction::EmitHeadExpr(void) {
+
+  assert(EdgeIndex && "no edges in call to head");
+
+  llvm::Value* Zero = llvm::ConstantInt::get(Int32Ty, 0);
+  llvm::Value* One = llvm::ConstantInt::get(Int32Ty, 1);
+  llvm::Value* Two = llvm::ConstantInt::get(Int32Ty, 2);
+  llvm::Value* Three = llvm::ConstantInt::get(Int32Ty, 3);
+
+  llvm::Value *Rank = Builder.CreateLoad(MeshRank);
+  llvm::Value *Index = Builder.CreateLoad(EdgeIndex, "forall.edgeIndex");
+  llvm::Value *Dx = Builder.CreateLoad(MeshDims[0]);
+  llvm::Value *Dx1 = Builder.CreateAdd(Dx,One);
+  llvm::Value *Dy = Builder.CreateLoad(MeshDims[1]);
+  llvm::Value *Dy1 = Builder.CreateAdd(Dy,One);
+
+  llvm::BasicBlock *Then3 = createBasicBlock("rank3.then");
+  llvm::BasicBlock *Else3 = createBasicBlock("rank3.else");
+  llvm::BasicBlock *Then2 = createBasicBlock("rank2.then");
+  llvm::BasicBlock *Else2 = createBasicBlock("rank2.else");
+  llvm::BasicBlock *Merge = createBasicBlock("rank.merge");
+
+  llvm::Value *Check3 = Builder.CreateICmpEQ(Rank, Three);
+  Builder.CreateCondBr(Check3, Then3, Else3);
+
+  // rank = 3
+  EmitBlock(Then3);
+  llvm::Value *Result3 =
+      llvm::UndefValue::get(llvm::VectorType::get(Int32Ty, 4));
+  //SC_TOD0: add correct x,y,z
+  Result3 = Builder.CreateInsertElement(Result3, Zero,
+      Builder.getInt32(0), "head.x");
+  Result3 = Builder.CreateInsertElement(Result3, Zero,
+      Builder.getInt32(1), "head.y");
+  Result3 = Builder.CreateInsertElement(Result3, Zero,
+      Builder.getInt32(2), "head.z");
+  Result3 = Builder.CreateInsertElement(Result3, Index,
+      Builder.getInt32(3), "head.idx");
+
+  Builder.CreateBr(Merge);
+
+  // rank != 3
+  EmitBlock(Else3);
+
+  // rank = 2
+  llvm::Value *Check2 = Builder.CreateICmpEQ(Rank, Two);
+  Builder.CreateCondBr(Check2, Then2, Else2);
+  EmitBlock(Then2);
+  llvm::Value *Result2 =
+      llvm::UndefValue::get(llvm::VectorType::get(Int32Ty, 4));
+
+  // number of edges in x direction
+  llvm::Value *nx = Builder.CreateMul(Dx1, Dy);
+  // what is direction for this Index
+  llvm::Value *dir = Builder.CreateICmpSLT(Index, nx);
+  llvm::Value *Index2 = Builder.CreateSub(Index, nx);
+
+  // for edges in x direction
+  llvm::Value *x1 = Builder.CreateSRem(Index, Dx1);
+  llvm::Value *y1 = Builder.CreateAdd(Builder.CreateSRem(Builder.CreateSDiv(Index, Dx1), Dy), One);
+  // for edges in y direction
+  llvm::Value *x2 = Builder.CreateAdd(Builder.CreateSRem(Index2, Dx), One);
+  llvm::Value *y2 = Builder.CreateSRem(Builder.CreateSDiv(Index2, Dx), Dy1);
+
+  llvm::Value *x = Builder.CreateSelect(dir, x1, x2);
+  llvm::Value *y = Builder.CreateSelect(dir, y1, y2);
+
+  Result2 = Builder.CreateInsertElement(Result2, x,
+      Builder.getInt32(0), "head.x");
+  Result2 = Builder.CreateInsertElement(Result2, y,
+      Builder.getInt32(1), "head.y");
+  Result2 = Builder.CreateInsertElement(Result2, Zero,
+      Builder.getInt32(2), "head.z");
+  Result2 = Builder.CreateInsertElement(Result2, Index,
+      Builder.getInt32(3), "head.idx");
+
+  Builder.CreateBr(Merge);
+
+  // rank !=2 (rank = 1)
+  EmitBlock(Else2);
+  llvm::Value *Index1 = Builder.CreateAdd(Index, One);
+  llvm::Value *Result1 =
+      llvm::UndefValue::get(llvm::VectorType::get(Int32Ty, 4));
+  Result1 = Builder.CreateInsertElement(Result1, Index1,
+      Builder.getInt32(0), "head.x");
+  Result1 = Builder.CreateInsertElement(Result1, Zero,
+      Builder.getInt32(1), "head.y");
+  Result1 = Builder.CreateInsertElement(Result1, Zero,
+      Builder.getInt32(2), "head.z");
+  Result1 = Builder.CreateInsertElement(Result1, Index1,
+      Builder.getInt32(3), "head.idx");
+
+  Builder.CreateBr(Merge);
+
+  // Merge Block
+  EmitBlock(Merge);
+  llvm::PHINode *PN = Builder.CreatePHI(llvm::VectorType::get(Int32Ty, 4), 3, "head.phi");
+  PN->addIncoming(Result3, Then3);
+  PN->addIncoming(Result2, Then2);
+  PN->addIncoming(Result1, Else2);
+
+  return RValue::get(PN);
+
 
 }
 
