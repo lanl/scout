6ffd608d29206191d56031749dfd13d0bee8fe2f
Nick Moss <nickm@lanl.gov>
2015-01-23 16:04:47 -0700

    Finish merge.

--- tools/lldb/lldb-prefix.h
+++ tools/lldb/lldb-prefix.h
@@ -1,7 +1,4 @@
 typedef float float4 __attribute__((ext_vector_type(4)));
 
-extern "C" {
-  float4 hsv(float, float, float);
-  float4 hsva(float, float, float, float);
-  void printf(const char *, ...);
-}
+float4 hsv(float, float, float);
+float4 hsva(float, float, float, float);
