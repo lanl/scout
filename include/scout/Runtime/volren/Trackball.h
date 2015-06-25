2a85679c242bc8ee768e449d76bea9bcee7bded3
Nick Moss <nickm@lanl.gov>
2015-06-25 14:46:38 -0600

    Update copyright notice on some files.

--- include/scout/Runtime/volren/Trackball.h
+++ include/scout/Runtime/volren/Trackball.h
@@ -1,37 +1,56 @@
-//
-//     _____                   __  
-//    / ___/__________  __  __/ /_ 
-//    \__ \/ ____/ __ \/ / /   __/
-//   ___/   /___/ /_/ / /_/ / /__ 
-//  /____/\____/\____/\____/\___/
-//
-//       Visualization Team 
-//     Advanced Computing Lab 
-//  Los Alamos National Laboratory
-// --------------------------------
-// 
-// $Id: Trackball.h,v 1.1 2005/07/22 18:27:30 groth Exp $
-// $Date: 2005/07/22 18:27:30 $
-// $Revision: 1.1 $
-//
-// Authors: A. McPherson, P. McCormick, J. Kniss, G. Roth
-//
-//.........................................................................
-// 
-//! Initially part of the ACL Visualization Library moved into 
-//! scout to clean things up some and remove external dependencies.
-//! Updated to the new 20-mule-team scout framework. Effectively gutted this
-//! part of it to alter the behavior somewhat.
-//! This class must service a number of different rotations so the internal
-//! state it maintained became a problem. Rather than clear the quaternions
-//! each time and start fresh, each update operations returns Rotations for
-//! us to do with as we will. In fact, we will hand them off to the Transform
-//! class which will composite them for the rotation part thereof.
-//! 
-//! Based on SGI's trackball.h---see copyright at end of this file.
-//
-//.........................................................................
-//
+/*
+ * ###########################################################################
+ * Copyright (c) 2015, Los Alamos National Security, LLC.
+ * All rights reserved.
+ *
+ *  Copyright 2015. Los Alamos National Security, LLC. This software was
+ *  produced under U.S. Government contract DE-AC52-06NA25396 for Los
+ *  Alamos National Laboratory (LANL), which is operated by Los Alamos
+ *  National Security, LLC for the U.S. Department of Energy. The
+ *  U.S. Government has rights to use, reproduce, and distribute this
+ *  software.  NEITHER THE GOVERNMENT NOR LOS ALAMOS NATIONAL SECURITY,
+ *  LLC MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR ASSUMES ANY LIABILITY
+ *  FOR THE USE OF THIS SOFTWARE.  If software is modified to produce
+ *  derivative works, such modified software should be clearly marked,
+ *  so as not to confuse it with the version available from LANL.
+ *
+ *  Additionally, redistribution and use in source and binary forms,
+ *  with or without modification, are permitted provided that the
+ *  following conditions are met:
+ *
+ *    * Redistributions of source code must retain the above copyright
+ *      notice, this list of conditions and the following disclaimer.
+ *
+ *    * Redistributions in binary form must reproduce the above
+ *      copyright notice, this list of conditions and the following
+ *      disclaimer in the documentation and/or other materials provided
+ *      with the distribution.
+ *
+ *    * Neither the name of Los Alamos National Security, LLC, Los
+ *      Alamos National Laboratory, LANL, the U.S. Government, nor the
+ *      names of its contributors may be used to endorse or promote
+ *      products derived from this software without specific prior
+ *      written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY LOS ALAMOS NATIONAL SECURITY, LLC AND
+ *  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL LOS ALAMOS NATIONAL SECURITY, LLC OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ *  USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ *  OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ *  OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ *  SUCH DAMAGE.
+ * ###########################################################################
+ *
+ * Notes
+ *
+ * #####
+ */
 
 #ifndef _TRACKBALL_H_
 #define _TRACKBALL_H_
