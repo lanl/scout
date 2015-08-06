74090df583005cbdc1aada1ff42232a1d97d241c
Xin Tong <tongxin829@gmail.com>
2015-08-06 11:30:05 -0600

    add copyright on the top of the source files.

--- lib/Runtime/volren/cuda_helper.h
+++ lib/Runtime/volren/cuda_helper.h
@@ -1,3 +1,19 @@
+//This file is derived from the NVIDIA CUDA SDK file "helper_cuda.h"
+
+/**
+ * Copyright 1993-2013 NVIDIA Corporation.  All rights reserved.
+ *
+ * Please refer to the NVIDIA end user license agreement (EULA) associated
+ * with this source code for terms and conditions that govern your use of
+ * this software. Any use, reproduction, disclosure, or distribution of
+ * this software and related documentation outside the terms of the EULA
+ * is strictly prohibited.
+ *
+ */
+
+
+
+
 //#include <iostream>
 //#include "cstdlib"
 //using namespace std;
